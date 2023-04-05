a865dfa225cf11246cba6b940b4f21c9fd7f86ad
Lu Cheng <75212732+chenglu29@users.noreply.github.com>
2023-04-04 20:27:09 -0400

    Add files via upload

---
+++ Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ajaxjquery2.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+        public static string GetCurrentTime(string name)
+        {
+            return "Hello " + name + Environment.NewLine + "The Current Time is: "
+                + DateTime.Now.ToString();
+        }
+        public IActionResult OnPostGetAjax(string name)
+        {
+            return new JsonResult("hello" + name);
+
+        }
+
+    }
+}
+
+
